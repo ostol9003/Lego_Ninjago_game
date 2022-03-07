954965e28344d33ea7380b7416c6e679bf012636
OstolskiM <ostolski.marcin@gmail.com>
2022-03-07 17:45:37 +0100

    Initial Commit

---
+++ Lego_Ninjago_game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lego_Ninjago_game
+//
+//  Created by Marcin Ostolski on 07/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
